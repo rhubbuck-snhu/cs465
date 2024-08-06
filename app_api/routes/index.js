a024b656c2049ecd87ff810ff86b00278972cb76
Ryan Hubbuck <dev.rhubbuck@gmail.com>
2024-08-06 07:33:35 -0400

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,10 +5,12 @@ const tripsController = require('../controllers/trips');
 
 router 
 .route('/trips')
-.get(tripsController.tripsList); // GET method routes tripList
+.get(tripsController.tripsList) // GET method routes tripList
+.post(tripsController.tripsAddTrip); // POST Method adds a trip
 
 router
 .route('/trips/:tripCode')
-.get(tripsController.tripsFindByCode); // GET method routes tripsFindByCode
+.get(tripsController.tripsFindByCode) // GET method routes tripsFindByCode
+.put(tripsController.tripsUpdateTrip); // Update method
 
 module.exports = router;
